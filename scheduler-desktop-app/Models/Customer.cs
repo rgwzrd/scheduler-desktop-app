9575422495c2a4e3450b63ef23619bbcdc74a192
Raffi G <raffi.gevorg@gmail.com>
2026-02-24 17:47:24 -0800

    feat: add customer edit form with validation save flow

--- scheduler-desktop-app/Models/Customer.cs
+++ scheduler-desktop-app/Models/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace scheduler_desktop_app.Models
 {
-    internal class Customer
+    public class Customer
     {
         public int CustomerId { get; set; }
         public string CustomerName { get; set; }
