67fddc2d0972adba6c523eb5aae71790ca47f3f7
Raffi G <raffi.gevorg@gmail.com>
2026-02-28 13:02:39 -0800

    feat: add appoinment model

---
+++ scheduler-desktop-app/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheduler_desktop_app.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int UserId { get; set; }
+        public string Type { get; set; }
+
+        public System.DateTime StartUtc { get; set; }
+        public DateTime EndUtc { get; set; }
+    }
+}
